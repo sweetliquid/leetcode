74d4b2e50a463054a0bebcd95df3e09cb76c86dd
Sweet Liquid <himejisyana@hotmail.com>
2020-09-21 09:38:11 +0800

    Add solution

---
+++ 32.最长有效括号.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=32 lang=javascript
+ *
+ * [32] 最长有效括号
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function (s) {
+  const f = parentheses(s)
+  let length = f(0)
+}
+
+const parentheses = (s, i) => {
+}
+// @lc code=end
